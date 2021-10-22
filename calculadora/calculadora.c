05c576a7b8a449c180e607550e365999b09cdd1b
devydk <78767451+devydk@users.noreply.github.com>
2021-10-22 02:57:13 -0300

    main

--- calculadora/calculadora.c
+++ calculadora/calculadora.c
@@ -21,13 +21,13 @@ int main(void)
     if (op == 1)
     {
         nmr1 = nmr1 + nmr2;
-        printf("O resultado da soma Ã©: %d", nmr1);
+        printf("O resultado da soma é: %d", nmr1);
 
     }
     else{
         nmr1 = nmr1 - nmr2;
 
-        printf("O resultado da subtraÃ§Ã£o Ã©: %d", nmr1);
+        printf("O resultado da subtração é: %d", nmr1);
     }
     
     return 0;
