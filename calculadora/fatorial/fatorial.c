19e135cc3551405e8f6e87a9e6c072302e8dec29
devydk <78767451+devydk@users.noreply.github.com>
2021-10-28 10:41:22 -0300

    code

---
+++ calculadora/fatorial/fatorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  int N, i;
+  int fatorial=1;
+
+  scanf("%d", &N);
+  for(i =1; i <= N; i++){
+    fatorial = fatorial*i;
+    printf("%d %d\n", i, fatorial);
+  }
+  return 0;
+}
