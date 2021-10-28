58e889aad136655286999abb32de27669a6011c6
devydk <78767451+devydk@users.noreply.github.com>
2021-10-28 16:48:09 -0300

    code

--- calculadora/valores entre dois números/valoresDoisNúmeros.c
+++ calculadora/valores entre dois números/valoresDoisNúmeros.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
+  int A, B, i, aux;
+
+  scanf("%d %d", &A, &B);
+
+  if (B <  A)
+  {
+    aux = B;
+    B = A;
+    A = aux;
+  }
   
+
+  for ( i = A; i <= B; i++);
+  {
+    printf("%d", i);
+  }
+  
+
   return 0;
 }
