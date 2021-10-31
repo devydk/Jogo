ca8fb49119dfdb7bdb917fce70de2f648f0c695f
devydk <78767451+devydk@users.noreply.github.com>
2021-10-31 19:38:22 -0300

    code

---
+++ calculadora/modularizar/modularizar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+retorno nome (parâmetros) {
+  códigos
+}
+*/
+int imprime ( ) {
+  printf("Devyd \n");
+}
+int main(int argc, char const *argv[])
+{
+  imprime ();
+  return 0;
+}
